3bd70cb805b8c5d22b57446a1c7c6c2737b04622
Vinay N <119772456+VinayNagaraj07@users.noreply.github.com>
2023-06-03 16:23:46 +0530

    Update app4.py

--- app4.py
+++ app4.py
@@ -41,56 +41,55 @@ if text:
 	blob = TextBlob(text)
 predict_button = st.button("Predict")
 #st.write(predict_button)
-if (predict_button==True):
-	if (text!=""):
-		wordcloud = WordCloud(width=800, height=400).generate(text)
-		st.title('Word Cloud')
-		plt.figure(figsize=(10, 5))
-		plt.imshow(wordcloud, interpolation='bilinear')
-		plt.axis('off')
-		st.pyplot(plt)
+if (text!=""):
+	wordcloud = WordCloud(width=800, height=400).generate(text)
+	st.title('Word Cloud')
+	plt.figure(figsize=(10, 5))
+	plt.imshow(wordcloud, interpolation='bilinear')
+	plt.axis('off')
+	st.pyplot(plt)
 
-		#st.write('Polarity: ', round(blob.sentiment.polarity,2))
-		#st.write('Subjectivity: ', round(blob.sentiment.subjectivity,2))
-		plt.figure(figsize = (20,20))
+	#st.write('Polarity: ', round(blob.sentiment.polarity,2))
+	#st.write('Subjectivity: ', round(blob.sentiment.subjectivity,2))
+	plt.figure(figsize = (20,20))
 
-	redunant_df = pd.read_csv("reduced_words.csv")
-	redunant=set(redunant_df["words"])
+redunant_df = pd.read_csv("reduced_words.csv")
+redunant=set(redunant_df["words"])
 
-	text1=cleaning_reduntant(text1)
-	if(text1!=""):
-		st.title("Cleaned Text")
-		text1 = re.sub('((www.[^s]+)|(https?://[^s]+))|(http?://[^s]+)', '',text1)
-		tknzr = TweetTokenizer(strip_handles=True)
-		text1=tknzr.tokenize(text1)
-		text1=str(text1)
-		text1=re.sub(r'[^a-zA-Z0-9\s]', '', text1)
-		text1=cleantext.clean(text1, clean_all= False, extra_spaces=True ,stopwords=True ,lowercase=True ,numbers=True , punct=True)
-		st.write(text1)
+text1=cleaning_reduntant(text1)
+if(text1!=""):
+	st.title("Cleaned Text")
+	text1 = re.sub('((www.[^s]+)|(https?://[^s]+))|(http?://[^s]+)', '',text1)
+	tknzr = TweetTokenizer(strip_handles=True)
+	text1=tknzr.tokenize(text1)
+	text1=str(text1)
+	text1=re.sub(r'[^a-zA-Z0-9\s]', '', text1)
+	text1=cleantext.clean(text1, clean_all= False, extra_spaces=True ,stopwords=True ,lowercase=True ,numbers=True , punct=True)
+	st.write(text1)
 
-	with open(filename, 'rb') as file:
-		model = pickle.load(file)
-		unseen_tweets=[text1]
-		unseen_df=pd.DataFrame(unseen_tweets)
-		unseen_df.columns=["Unseen"]
+with open(filename, 'rb') as file:
+	model = pickle.load(file)
+	unseen_tweets=[text1]
+	unseen_df=pd.DataFrame(unseen_tweets)
+	unseen_df.columns=["Unseen"]
 
-	X_test = vectorizer.transform(unseen_tweets)
-	y_pred = model.predict(X_test)
+X_test = vectorizer.transform(unseen_tweets)
+y_pred = model.predict(X_test)
 
-	if text!="":
-		if(y_pred==0):
-			remark = "That's Figurative!😄"
-			display_sarcastic_remark(remark)
-		if(y_pred==1):
-			remark = "That's Irony!😏"
-			display_sarcastic_remark(remark)
-		if(y_pred==2):
-			remark = "That's Regular!😐"
-			display_sarcastic_remark(remark)
-		if(y_pred==3):
-			remark = "That's Sarcasm!🙃"
-			display_sarcastic_remark(remark)
-	else:
-		st.write(text1)
-		remark = "No Words to Analyze"
+if text!="":
+	if(y_pred==0):
+		remark = "That's Figurative!😄"
 		display_sarcastic_remark(remark)
+	if(y_pred==1):
+		remark = "That's Irony!😏"
+		display_sarcastic_remark(remark)
+	if(y_pred==2):
+		remark = "That's Regular!😐"
+		display_sarcastic_remark(remark)
+	if(y_pred==3):
+		remark = "That's Sarcasm!🙃"
+		display_sarcastic_remark(remark)
+else:
+	st.write(text1)
+	remark = "No Words to Analyze"
+	display_sarcastic_remark(remark)
