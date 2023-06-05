85b8e0f99cc914a2b8cbca9767c05c5d869842df
Vinay N <119772456+VinayNagaraj07@users.noreply.github.com>
2023-06-05 11:46:56 +0530

    Update app4.py

--- app4.py
+++ app4.py
@@ -71,8 +71,17 @@ if(text1!=""):
 
 
 annotated_text(
-    "Hello ",
-    annotation("world!", "Stop Word", color="#faa"),
+    "This ",
+    ("is", "verb", "#8ef"),
+    " some ",
+    ("annotated", "adj", "#faa"),
+    ("text", "noun", "#afa"),
+    " for those of ",
+    ("you", "pronoun", "#fea"),
+    " who ",
+    ("like", "verb", "#8ef"),
+    " this sort of ",
+    ("thing", "noun", "#afa"),
 )
 with open(filename, 'rb') as file:
 	model = pickle.load(file)
