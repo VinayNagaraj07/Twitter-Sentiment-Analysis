6452f980b0a23e906c6142457575c2c91a5bbc00
Vinay N <119772456+VinayNagaraj07@users.noreply.github.com>
2023-06-05 08:21:46 +0530

    Update app4.py

--- app4.py
+++ app4.py
@@ -105,18 +105,16 @@ if button:
     modal.open()
 import streamlit.components.v1 as components
 if modal.is_open():
-    with modal.container():
-	st.write("Text goes here")
-
-        html_string = '''
+	with modal.container():
+		st.write("Text goes here")
+		html_string = '''
         <h1>HTML string in RED</h1>
 
         <script language="javascript">
           document.querySelector("h1").style.color = "red";
         </script>
         '''
-        components.html(html_string)
-
-        st.write("Some fancy text")
-        value = st.checkbox("Check me")
-        st.write(f"Checkbox checked: {value}")
+		components.html(html_string)
+		st.write("Some fancy text")
+		value = st.checkbox("Check me")
+		st.write(f"Checkbox checked: {value}")
